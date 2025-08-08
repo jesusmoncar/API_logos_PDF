92d8625c06484ff86fadf1783feb2f2fa0f25ba3
jesusmoncar <jesus.montero.jmc@gmail.com>
2025-08-08 17:37:31 +0200

    modificado carpetas de entradas y salidas y limpiador_carpetas

--- limpiar_carpetas.bat
+++ limpiar_carpetas.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Eliminando contenido de las carpetas PDF y pdfModificado...
-del /q "PDF\*.*"
-del /q "pdfModificado\*.*"
+del /q "input\*.*"
+del /q "outputModificado\*.*"
 echo Carpetas limpiadas exitosamente.
 pause
